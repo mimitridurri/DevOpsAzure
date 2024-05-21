5d2148089275f618cc67a683ff301622a7044c11
Dimitri <murridim@students.zhaw.ch>
2024-05-21 15:49:29 +0200

    check if it is still not working

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps FS2024 :-)) CHANGES by Murridim!');
+  res.send('Hello DevOps FS2024 :-)) CHANGES by Murridim!!');
 });
 
 app.listen(PORT, HOST);
